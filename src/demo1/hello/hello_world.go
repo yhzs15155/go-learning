35fff6f3b88cfb11eb46b8c295eaba5176e931bc
Yinghao <yhzs15155@gmail.com>
2020-08-12 15:27:51 +0800

    U

--- src/demo1/hello/hello_world.go
+++ src/demo1/hello/hello_world.go
@@ -10,5 +10,15 @@ func main(){
 	if len(os.Args) > 1{
 		fmt.Println("Hello world", os.Args[1])
 	}
+
+	var a = 1
+
+	fmt.Println(a)
+
+	// 循环
+	for i := 0; i <= 10; i ++ {
+
+	}
+
 	os.Exit(-1)
 }
--- src/demo2/fib/fib_test.go
+++ src/demo2/fib/fib_test.go
@@ -8,11 +8,11 @@ func TestFibList(t *testing.T) {
 	var a int = 1
 	var b int = 1
 	t.Log(a, " ")
-	for i:=0;i<5;i++ {
+	for i := 0; i < 5; i ++ {
 		t.Log(" ", b)
-		tmp:=a
-		a=b
-		b=tmp+a
+		tmp := a
+		a = b
+		b = tmp + a
 	}
 	t.Log()
 }
